e7caafa0e9d9a3d87f6da50efb835e46a7ddf64d
Mario Vidov <mvidov@yahoo.com>
2024-01-06 17:15:00 +0100

    Update lib documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Ray tracing library
 //!
 //! This low-level library contains all that you need to develop all kinds off ray tracers.
-//! It has random number generator, 3D vector math library, ...
+//! It has random number generator, 3D vector math library. 
 
 pub mod rng;
